c043893c3b4a47dd41542802a569189a63e07c2e
Betsy Mendoza Gomez <betsai.mendozagomez@gmail.com>
2020-05-05 00:13:18 -0500

    delete routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
-import Shop from './components/Shop';
-import About from './components/About';
 import Home from './components/home';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
@@ -11,11 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
-        <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/shop" component={Shop}/>
-        </Switch>
+        <Home />
       </div>
     </Router>
   );
