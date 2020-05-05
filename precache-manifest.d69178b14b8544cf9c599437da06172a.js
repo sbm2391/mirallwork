f96dd70a72a81acfef2927148f034f7b2845fad9
Betsy Mendoza Gomez <betsai.mendozagomez@gmail.com>
2020-05-05 00:09:22 -0500

    Updates

---
+++ precache-manifest.d69178b14b8544cf9c599437da06172a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4945f18d3242d978429c2029578aebae",
+    "url": "/mirallwork/index.html"
+  },
+  {
+    "revision": "bb0d30f80ec304e684c9",
+    "url": "/mirallwork/static/css/main.eb931041.chunk.css"
+  },
+  {
+    "revision": "de53360d4ded5bae5d82",
+    "url": "/mirallwork/static/js/2.edf49313.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mirallwork/static/js/2.edf49313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb0d30f80ec304e684c9",
+    "url": "/mirallwork/static/js/main.62e1098d.chunk.js"
+  },
+  {
+    "revision": "394f144974cf27a68015",
+    "url": "/mirallwork/static/js/runtime-main.81639f83.js"
+  },
+  {
+    "revision": "b3f451aab275d80dd7e8588aa8853ad0",
+    "url": "/mirallwork/static/media/artIcon.b3f451aa.svg"
+  },
+  {
+    "revision": "85876ba22d6968b378cd2bce11114c0c",
+    "url": "/mirallwork/static/media/bigCircle.85876ba2.svg"
+  },
+  {
+    "revision": "d5961ad126afdaa120f4234834e589bb",
+    "url": "/mirallwork/static/media/bigTriangle.d5961ad1.svg"
+  },
+  {
+    "revision": "fe43d6a504f5c83a0696031460db951b",
+    "url": "/mirallwork/static/media/designIcon.fe43d6a5.svg"
+  },
+  {
+    "revision": "594694daede651b6a8ee98a752643470",
+    "url": "/mirallwork/static/media/dotGrid.594694da.svg"
+  },
+  {
+    "revision": "92c9970b81084c3a02305810019fd666",
+    "url": "/mirallwork/static/media/entrepreneurshipIcon.92c9970b.svg"
+  },
+  {
+    "revision": "acb1942ba51c0dabcb403838647bb96a",
+    "url": "/mirallwork/static/media/fitnessIcon.acb1942b.svg"
+  },
+  {
+    "revision": "d9d6bee5e56ea66662d9a3ee1cdbf934",
+    "url": "/mirallwork/static/media/homeBackground.d9d6bee5.png"
+  },
+  {
+    "revision": "39e976e03c275556696887685d09c4c2",
+    "url": "/mirallwork/static/media/kitchenIcon.39e976e0.svg"
+  },
+  {
+    "revision": "73beeff87f115ca135a8fa6afa1e59c6",
+    "url": "/mirallwork/static/media/legalIcon.73beeff8.svg"
+  },
+  {
+    "revision": "5913ccf422b95a4126fb0f7be6cac6bd",
+    "url": "/mirallwork/static/media/lenguageIcon.5913ccf4.svg"
+  },
+  {
+    "revision": "cffa7ecab126a55524408764177b4bf2",
+    "url": "/mirallwork/static/media/marketingIcon.cffa7eca.svg"
+  },
+  {
+    "revision": "eb8651c1eda679883f19b62ea9c5e4bf",
+    "url": "/mirallwork/static/media/middleTriangle.eb8651c1.svg"
+  },
+  {
+    "revision": "09b6af36e2ff3c9560d12a70afd65aad",
+    "url": "/mirallwork/static/media/serviceSale.09b6af36.png"
+  },
+  {
+    "revision": "0b1939e39ea9f270380997ca2719c3a2",
+    "url": "/mirallwork/static/media/triangle.0b1939e3.svg"
+  }
+]);
